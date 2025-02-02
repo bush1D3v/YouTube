d80a765a029b671128b45f9615497706e44286a6
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2025-02-02 15:48:56 -0300

    feat: Code related to the video

---
+++ Módulo de JavaScript/Estrutura de Condicionais no JavaScript - Aula 06 - Curso de javaScript/Código do Vídeo/index.js
@@ -0,0 +1,30 @@
+// const idade = 18;
+
+// if (idade >= 18) {
+//     console.log("Maior de idade");
+// } else {
+//     console.log("Menor de idade");
+// }
+
+// const idade = 18;
+
+// if (idade >= 18 && idade < 65) {
+//     console.log("Tem que votar");
+// } else if (idade < 16) {
+//     console.log("Nao pode votar");
+// } else if (idade >= 16 && idade < 18) {
+//     console.log("Voce PODE votar");
+// } else if (idade >= 65) {
+//     console.log("Voce PODE votar");
+// }
+
+// const num1 = 10;
+// const num2 = 5;
+
+// console.log(num1 % num2 === 0 ? "Par" : "Impar");
+
+// if (num1 % num2 === 0) {
+//     console.log("Par");
+// } else {
+//     console.log("Impar");
+// }
