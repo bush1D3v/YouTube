62085e4ee2f679b90a0d40f8e285634cf6471e8e
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2025-02-02 15:36:26 -0300

    feat: Add code related to the video

---
+++ Módulo de JavaScript/Operadores Matemétivos e Comparativos no JavaScript - Aula 05 - Curso de JavaScript/Código do Vídeo/index.js
@@ -0,0 +1,54 @@
+// Operadores Matemáticos
+
+// const num1 = 5;
+// const num2 = 10;
+
+// console.log("num1 + num2");
+// console.log(num1 + num2);
+
+// console.log("num2 - num1");
+// console.log(num2 - num1);
+
+// /**
+//  * @type number
+//  */
+// const num1 = 10;
+
+// /**
+//  * @type string
+//  */
+// const fakeNum = "5";
+
+// console.log(num1 + fakeNum);
+
+// const num1 = 5;
+// const num2 = 10;
+
+// console.log(num1 * num2);
+
+// const num1 = 5;
+// const num2 = 10;
+
+// console.log(num2 / num1); 
+
+// const num1 = 10;
+// const num2 = 4;
+
+// console.log(num1 % num2);
+
+// Operadores de comparação
+
+// console.log(10 == "10");
+// console.log(10 === 10);
+
+// console.log(10 != "10");
+// console.log(10 !== "10");
+
+// console.log(5 > 8);
+// console.log(5 < 8);
+// console.log(5 >= 5);
+// console.log(5 <= 4);
+
+// const numero = 6;
+
+// console.log(numero % 2 === 0 ? "Par" : "Impar");
