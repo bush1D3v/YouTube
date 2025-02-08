e54bac45fdf7fc8a5308d0b92569542c2f86d007
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2025-02-08 15:13:43 -0300

    feat: Code related to the video

---
+++ Módulo de JavaScript/Estruturas de Repetição no JavaScript - Aula 07 - Curso de JavaScript/index.js
@@ -0,0 +1,28 @@
+// // Estrutura do For
+
+// for (let i = 0; i < 5; i++) {
+//     console.log(i);
+// }
+
+// for (let index = 0; index < array.length; index++) {
+//     const element = array[index];  
+// }
+
+// // Estrutura do While
+
+// let a = 10;
+// const b = 15;
+
+// while (a < b) {
+//     console.log(`Incrimentando a variavel: ${a}`);
+//     a++;
+// }
+
+// // Estrutura do Do While
+
+// let i = 100;
+
+// do {
+//     console.log(i);
+//     i++;
+// } while (i < 5);
