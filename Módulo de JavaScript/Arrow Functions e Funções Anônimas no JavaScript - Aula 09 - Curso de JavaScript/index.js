9292e8e81cc17e049abde17ebc2db513f13b1b6d
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2025-02-17 19:26:00 -0300

    feat: Add code related to the video

---
+++ Módulo de JavaScript/Arrow Functions e Funções Anônimas no JavaScript - Aula 09 - Curso de JavaScript/index.js
@@ -0,0 +1,19 @@
+// // Arrow function
+
+// const frutas = ["Maçã", "Pera", "Uva", "Melancia"];
+
+// console.log(frutas.map(
+//     (fruta) => `A fruta ${fruta} tem um total de ${fruta.length} caracteres.`)
+// );
+
+// const teste = (params) => {
+//     //code
+// }
+
+// // Anonymous Functions
+
+// const numeros = [1, 2, 3, 4, 5]
+
+// const numerosDobrados = numeros.map((numero) => {
+//     return numero * 2;
+// });
