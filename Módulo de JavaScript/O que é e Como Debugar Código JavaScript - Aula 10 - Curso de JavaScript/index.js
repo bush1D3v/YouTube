7cd7b9800e7f856841a7fe2afa2a02960599fa34
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2025-02-17 19:50:10 -0300

    feat: Add code related to the video

---
+++ Módulo de JavaScript/O que é e Como Debugar Código JavaScript - Aula 10 - Curso de JavaScript/index.js
@@ -0,0 +1,30 @@
+// // Depuração de código
+
+// // Console log 
+
+// const nome = "Navarro"
+// console.log(nome);
+
+// // Sources do navegador
+
+// function soma(n1, n2) {
+//     debugger;
+//     return n1 + n2;
+// }
+
+// console.log(soma(5, 10));
+
+// debugger;
+
+// console.log("Passou do debugger 2");
+
+// // Debugger do VSCode
+
+// function soma(n1, n2) {
+//breakpoint here
+//     return n1 + n2;
+// }
+
+//breakpoint here console.log(soma(5, 10));
+
+// console.log("Passou do debugger 2");
