2cf4f5d6240bc54db20b427485ea38c08dc1c46f
Victor José Lopes Navarro <victor.jose.lopes.navarro@gmail.com>
2026-01-08 03:11:41 +0000

    feat: add code related to the video

---
+++ Módulo de Vue.js/Inicializando Projeto e Compreendendo Conceitos Base - Aula 02 - Curso de Vue.js/code/src/App.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import Button from './components/Button.vue';
+</script>
+
+<template>
+  <main>
+    <h1>Bem-vindo ao contador</h1>
+    <Button />
+  </main>
+</template>
