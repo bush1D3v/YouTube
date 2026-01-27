959c7ac3636c902426e65336686ee21df1aabcd1
Victor José Lopes Navarro <victor.jose.lopes.navarro@gmail.com>
2026-01-27 23:16:51 +0000

    add codes related to the videos

---
+++ Módulo de Vue.js/Aula 04/code/src/components/Button.vue
@@ -0,0 +1,24 @@
+<script lang="ts" setup>
+import { ref } from 'vue';
+
+const count = ref(0)
+
+function increment() {
+    count.value++;
+}
+</script>
+
+<template>
+    <button class="count-btn" aria-label="Botão Contador" title="Botão Contador" @click="increment" >
+        Cliquei {{ count }} vezes
+    </button>
+</template>
+
+<style lang="css" scoped>
+.count-btn {
+    padding: 8px;
+    border-radius: 8px;
+    cursor: pointer;
+    background: #13dcee;
+}
+</style>
